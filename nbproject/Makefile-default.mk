ef53539f8b74e0d3ad350145e2069bad0fede13f
RodrigoNdeC <rodrigondec@gmail.com>
2016-09-10 02:22:42 -0300

    timerv1 funcionando

--- nbproject/Makefile-default.mk
+++ nbproject/Makefile-default.mk
@@ -51,17 +51,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=/home/rodrigo/Documents/Prog/walle/main.c
+SOURCEFILES_QUOTED_IF_SPACED=/home/rodrigo/Documents/Prog/walle/PWMcomTimerV1.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/_ext/1237032825/main.p1
-POSSIBLE_DEPFILES=${OBJECTDIR}/_ext/1237032825/main.p1.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/_ext/1237032825/PWMcomTimerV1.p1
+POSSIBLE_DEPFILES=${OBJECTDIR}/_ext/1237032825/PWMcomTimerV1.p1.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/_ext/1237032825/main.p1
+OBJECTFILES=${OBJECTDIR}/_ext/1237032825/PWMcomTimerV1.p1
 
 # Source Files
-SOURCEFILES=/home/rodrigo/Documents/Prog/walle/main.c
+SOURCEFILES=/home/rodrigo/Documents/Prog/walle/PWMcomTimerV1.c
 
 
 CFLAGS=
@@ -87,22 +87,22 @@ MP_PROCESSOR_OPTION=18F45K20
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: compile
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
-${OBJECTDIR}/_ext/1237032825/main.p1: /home/rodrigo/Documents/Prog/walle/main.c  nbproject/Makefile-${CND_CONF}.mk
+${OBJECTDIR}/_ext/1237032825/PWMcomTimerV1.p1: /home/rodrigo/Documents/Prog/walle/PWMcomTimerV1.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}/_ext/1237032825" 
-	@${RM} ${OBJECTDIR}/_ext/1237032825/main.p1.d 
-	@${RM} ${OBJECTDIR}/_ext/1237032825/main.p1 
-	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  -D__DEBUG=1 --debugger=pickit3  --double=24 --float=24 --emi=wordwrite --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=-3 --asmlist -DXPRJ_default=$(CND_CONF)  --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,-plib $(COMPARISON_BUILD)  --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/_ext/1237032825/main.p1  /home/rodrigo/Documents/Prog/walle/main.c 
-	@-${MV} ${OBJECTDIR}/_ext/1237032825/main.d ${OBJECTDIR}/_ext/1237032825/main.p1.d 
-	@${FIXDEPS} ${OBJECTDIR}/_ext/1237032825/main.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	@${RM} ${OBJECTDIR}/_ext/1237032825/PWMcomTimerV1.p1.d 
+	@${RM} ${OBJECTDIR}/_ext/1237032825/PWMcomTimerV1.p1 
+	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  -D__DEBUG=1 --debugger=pickit3  --double=24 --float=24 --emi=wordwrite --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=-3 --asmlist -DXPRJ_default=$(CND_CONF)  --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,-plib $(COMPARISON_BUILD)  --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/_ext/1237032825/PWMcomTimerV1.p1  /home/rodrigo/Documents/Prog/walle/PWMcomTimerV1.c 
+	@-${MV} ${OBJECTDIR}/_ext/1237032825/PWMcomTimerV1.d ${OBJECTDIR}/_ext/1237032825/PWMcomTimerV1.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/_ext/1237032825/PWMcomTimerV1.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 else
-${OBJECTDIR}/_ext/1237032825/main.p1: /home/rodrigo/Documents/Prog/walle/main.c  nbproject/Makefile-${CND_CONF}.mk
+${OBJECTDIR}/_ext/1237032825/PWMcomTimerV1.p1: /home/rodrigo/Documents/Prog/walle/PWMcomTimerV1.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}/_ext/1237032825" 
-	@${RM} ${OBJECTDIR}/_ext/1237032825/main.p1.d 
-	@${RM} ${OBJECTDIR}/_ext/1237032825/main.p1 
-	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  --double=24 --float=24 --emi=wordwrite --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=-3 --asmlist -DXPRJ_default=$(CND_CONF)  --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,-plib $(COMPARISON_BUILD)  --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/_ext/1237032825/main.p1  /home/rodrigo/Documents/Prog/walle/main.c 
-	@-${MV} ${OBJECTDIR}/_ext/1237032825/main.d ${OBJECTDIR}/_ext/1237032825/main.p1.d 
-	@${FIXDEPS} ${OBJECTDIR}/_ext/1237032825/main.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	@${RM} ${OBJECTDIR}/_ext/1237032825/PWMcomTimerV1.p1.d 
+	@${RM} ${OBJECTDIR}/_ext/1237032825/PWMcomTimerV1.p1 
+	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  --double=24 --float=24 --emi=wordwrite --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=-3 --asmlist -DXPRJ_default=$(CND_CONF)  --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,-plib $(COMPARISON_BUILD)  --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/_ext/1237032825/PWMcomTimerV1.p1  /home/rodrigo/Documents/Prog/walle/PWMcomTimerV1.c 
+	@-${MV} ${OBJECTDIR}/_ext/1237032825/PWMcomTimerV1.d ${OBJECTDIR}/_ext/1237032825/PWMcomTimerV1.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/_ext/1237032825/PWMcomTimerV1.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 endif
 
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=walle.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/walle.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=walle.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/walle.tar
